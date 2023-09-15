46582fa4feb6d200bd22591e23c216d90826f720
hshinde@rdalabs.com <hshinde@rdalabs.com>
2023-09-15 21:09:53 +0530

    initial commit

--- src/main/java/com/leetcode/study/dsa/twodarray/PascalsTriangle.java
+++ src/main/java/com/leetcode/study/dsa/twodarray/PascalsTriangle.java
@@ -11,27 +11,14 @@ public class PascalsTriangle {
 
     public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> output = new ArrayList<>();
-
         for (int i = 0; i < numRows; i++) {
             List<Integer> list = new ArrayList<>(i);
-            int count = 0;
-            while (count <= i) {
-                if(count == 0 || count == i) {
-                    list.add(1);
-                } else {
-                    list.add(0);
-                }
-                count++;
-            }
-
+            list.add(1);
             for (int j = 1; j < i; j++) {
-
-               list.set(j, output.get(i-1).get(j-1) + output.get(i-1).get(j));
-
+               list.add(output.get(i-1).get(j-1) + output.get(i-1).get(j));
             }
-
-            output.add(i, list);
-
+            list.add(1);
+            output.add(list);
         }
         return output;
 
